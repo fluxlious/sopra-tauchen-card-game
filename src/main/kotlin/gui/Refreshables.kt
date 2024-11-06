7a2caa3faa0d59efafe00ca03f2f01d42ab089b1
fluxyc <38401401+fluxyc@users.noreply.github.com>
2024-11-06 22:20:51 +0100

    Service layer partially implemented

---
+++ src/main/kotlin/gui/Refreshables.kt
@@ -0,0 +1,28 @@
+package service
+import entity.Card
+interface Refreshables {
+
+    fun refreshAfterGameStart(){}
+
+
+    fun refreshAfterTurnStart(){}
+
+
+    fun refreshAfterTurnEnds(){}
+
+
+    fun refreshAfterCardPlayed(playedCard: Card){}
+
+
+    fun refreshAfterCardDrawn(card: Card){}
+
+
+    fun refreshAfterCardSwap(trioCard: Card, handCard: Card){}
+
+
+    fun refreshAfterCardDiscarded(handCard: Card){}
+
+
+    fun refreshAfterGameEnds(){}
+
+}
